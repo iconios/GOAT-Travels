2892897a07d50bb7cb1461f5037036b00cbc978a
Ademuyiwa Ikotun <ademuyiwaikotun@gmail.com>
2024-08-09 22:09:31 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html, js}"],
+  theme: {
+    colors: {
+      'primary': '#1A7465',
+      'secondary': '#F7C763'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
